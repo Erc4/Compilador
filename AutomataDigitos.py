3f1cd124b5d1e57dbb02647c8af841f1ed028424
Erc4 <ericamezcua3@gmail.com>
2024-09-18 08:50:53 -0600

    Creación del repositorio

---
+++ AutomataDigitos.py
@@ -0,0 +1,29 @@
+class AutomataDigitos:
+    def __init__(self):
+        self.estado = 0
+
+    def procesarcadena(self, cadena):
+        if len(cadena) != 1:  
+            return False
+
+        #| q0 | --- {0..9} --- || q1 ||
+
+        for caracter in cadena:
+            if self.estado == 0:              # q0
+                if caracter.isdigit():
+                    self.estado = 1           # q1
+                else:
+                    self.estado = -1
+        return self.estado == 1
+
+automata = AutomataDigitos()
+
+cadena = "3"
+if automata.procesarcadena(cadena):
+    print("Cadena aceptada")
+else:
+    print("Cadena no aceptada")
+
+
+                    
+
